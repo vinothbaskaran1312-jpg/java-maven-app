37ec3f4855d4611be24581c2a078ac07e874073f
vinothbaskaran1312-jpg <vinoth.baskaran1312@gmail.com>
2026-07-15 22:15:59 +0530

    V1 updated

--- src/main/java/com/vinoth/app/HelloController.java
+++ src/main/java/com/vinoth/app/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Java Maven App! Built by Jenkins CI/CD Pipeline";
+        return "Hello from Java Maven App! Built by Jenkins CI/CD Pipeline - V1";
     }
 
     @GetMapping("/health")
